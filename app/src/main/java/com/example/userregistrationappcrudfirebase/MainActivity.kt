334dc97076e75289b38d147ccf3e20779bae01be
reisalazar <rrsb45@gmail.com>
2023-01-27 18:34:28 -0300

    retrieving data from firebase RT database 2

--- app/src/main/java/com/example/userregistrationappcrudfirebase/MainActivity.kt
+++ app/src/main/java/com/example/userregistrationappcrudfirebase/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.userregistrationappcrudfirebase
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.userregistrationappcrudfirebase.databinding.ActivityMainBinding
 import com.google.firebase.database.*
 
@@ -10,8 +11,11 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var mainBinding: ActivityMainBinding
 
-    val database : FirebaseDatabase = FirebaseDatabase.getInstance()
-    val myReference : DatabaseReference = database.reference.child("MyUsers")
+    val database: FirebaseDatabase = FirebaseDatabase.getInstance()
+    val myReference: DatabaseReference = database.reference.child("MyUsers")
+
+    val userList = ArrayList<Users>()
+    lateinit var usersAdapter: UsersAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,24 +30,33 @@ class MainActivity : AppCompatActivity() {
         }
 
 
-retrieveDataFromDatabase()
+        retrieveDataFromDatabase()
 
     }
 
-    fun retrieveDataFromDatabase(){
-        myReference.addValueEventListener(object : ValueEventListener{
+    fun retrieveDataFromDatabase() {
+        myReference.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
 
-                for(eachUser in snapshot.children){
+                userList.clear()
+
+                for (eachUser in snapshot.children) {
                     val user = eachUser.getValue(Users::class.java)
 
-                    if (user != null ){
+                    if (user != null) {
                         println("userId: ${user.userId}")
                         println("userName: ${user.userName}")
                         println("userAge: ${user.userAge}")
                         println("userEmail: ${user.userEmail}")
                         println("****************************")
+
+                        userList.add(user)
+
                     }
+
+                    usersAdapter = UsersAdapter(this@MainActivity, userList)
+                    mainBinding.recyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
+                    mainBinding.recyclerView.adapter = usersAdapter
 
                 }
 
---
+++ app/src/main/java/com/example/userregistrationappcrudfirebase/UsersAdapter.kt
@@ -0,0 +1,37 @@
+package com.example.userregistrationappcrudfirebase
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.userregistrationappcrudfirebase.databinding.UsersItemBinding
+
+class UsersAdapter(
+    var context: Context,
+    var userList: ArrayList<Users>
+) : RecyclerView.Adapter<UsersAdapter.UsersViewHolder>() {
+
+
+    inner class UsersViewHolder(val adapterBinding: UsersItemBinding) :
+        RecyclerView.ViewHolder(adapterBinding.root) {}
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UsersViewHolder {
+
+        val binding = UsersItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return UsersViewHolder(binding)
+
+    }
+
+    override fun onBindViewHolder(holder: UsersViewHolder, position: Int) {
+
+        holder.adapterBinding.textViewName.text = userList[position].userName
+        holder.adapterBinding.textViewAge.text = userList[position].userAge.toString()
+        holder.adapterBinding.textViewEmail.text = userList[position].userEmail
+
+    }
+
+    override fun getItemCount(): Int {
+        return userList.size
+    }
+
+}
