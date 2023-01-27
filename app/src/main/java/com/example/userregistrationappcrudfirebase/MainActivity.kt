2bf1c040ea3c0c1811cf91530e88c5a7c858c05e
reisalazar <rrsb45@gmail.com>
2023-01-27 17:53:10 -0300

    retrieving data from firebase RT database

--- app/src/main/java/com/example/userregistrationappcrudfirebase/MainActivity.kt
+++ app/src/main/java/com/example/userregistrationappcrudfirebase/MainActivity.kt
@@ -4,10 +4,15 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.userregistrationappcrudfirebase.databinding.ActivityMainBinding
-
-lateinit var mainBinding: ActivityMainBinding
+import com.google.firebase.database.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mainBinding: ActivityMainBinding
+
+    val database : FirebaseDatabase = FirebaseDatabase.getInstance()
+    val myReference : DatabaseReference = database.reference.child("MyUsers")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
@@ -19,5 +24,35 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, AddUserActivity::class.java)
             startActivity(intent)
         }
+
+
+retrieveDataFromDatabase()
+
     }
+
+    fun retrieveDataFromDatabase(){
+        myReference.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+                for(eachUser in snapshot.children){
+                    val user = eachUser.getValue(Users::class.java)
+
+                    if (user != null ){
+                        println("userId: ${user.userId}")
+                        println("userName: ${user.userName}")
+                        println("userAge: ${user.userAge}")
+                        println("userEmail: ${user.userEmail}")
+                        println("****************************")
+                    }
+
+                }
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
+    }
+
 }
